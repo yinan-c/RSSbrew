f6c7d1aa3a883ac77d6cf59881f087a4b07c499f
Yinan <95043151+yinan-c@users.noreply.github.com>
2025-09-02 09:50:37 +0100

    feat: added a navigation button in digest view to digest list view

--- FeedManager/views.py
+++ FeedManager/views.py
@@ -43,6 +43,7 @@ def digest_html_view(request, feed_name, date_str):
         "feed_name": digest.processed_feed.name,
         "feed_id": digest.processed_feed.id,
         "date_range": date_range,
+        "auth_key": auth_code if auth_code else "",
     }
 
     return render(request, "FeedManager/digest.html", context)
